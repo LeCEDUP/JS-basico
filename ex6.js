42ff273607d93120ed48d6da44f06923be4da273
Alicite <85319704+Alicite@users.noreply.github.com>
2025-09-01 19:37:53 -0300

    Create ex6.js

---
+++ ex6.js
@@ -0,0 +1,10 @@
+// 1. Selecione o elemento h1 pelo seu ID e mude seu textContent para "DOM Manipulado!".
+
+// 2. Selecione o parágrafo pela sua classe e adicione a classe CSS "destaque" a ele.
+
+// 3. Selecione o botão pelo seu ID. Adicione um event listener de 'click' a ele.
+
+// Quando o botão for clicado, mude o textContent do parágrafo para "Texto alterado pelo clique!".
+
+// 4. Crie um novo elemento <p> com o textContent "Este é um novo parágrafo criado via JS!".
+// Adicione este novo parágrafo ao elemento <div> com o ID "containernovo-elemento"
