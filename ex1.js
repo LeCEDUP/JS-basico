265e528b1d072eb00dc9361a33d94f612eaf2c0e
olliiveirazx <mauriciojunior2584697@gmail.com>
2025-09-08 21:25:34 -0300

    Exercicio 1 - Resolvido

---
+++ ex1.js
@@ -0,0 +1,10 @@
+let nome = "MauMau"
+const anoNascimento = 2006;
+let anoAtual = 2025;
+let idade = anoAtual- anoNascimento;
+let isestudante = true;
+
+console.log("Nome:", nome, "(", typeof nome, ")");
+console.log("Ano de Nascimento:", anoNascimento, "(", typeof anoNascimento, ")");
+console.log("Idade:", idade, "(", typeof idade, ")");
+console.log("É estudante?", isestudante, "(", typeof isestudante, ")");
