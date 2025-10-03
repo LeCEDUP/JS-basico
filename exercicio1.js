3e0cde2c54da9f5e19716f6d6022b1e4a9adf130
caioonofre <protectcaio@gmail.com>
2025-10-03 18:00:06 -0300

    feat: alterações no exercicio 1 e exercicio 2 concluido

--- exercicio1.js
+++ exercicio1.js
@@ -1,37 +1,15 @@
 // ## Exercício 1: Array e Loop - Encontrar o Maior Número
 
-// ### Descrição
-// Crie uma função chamada `encontrarMaiorNumero` que recebe um array de números como argumento. A função deve retornar o maior número presente no array.
+const numeros = [55, 1, 10, 452, 1001, 1000];
 
-// ### Exemplo de Entrada
-// ```javascript
-// const numeros = [15, 8, 23, 4, 42, 16];
-// ```
-
-// ### Exemplo de Saída Esperada
-// ```javascript
-// 42
-// ```
-
-// ### Dicas
-// *   Inicialize uma variável para armazenar o maior número encontrado até o momento.
-// *   Use um loop `for` para iterar sobre cada elemento do array.
-// *   Dentro do loop, utilize uma estrutura condicional (`if`) para comparar o número atual com o maior número armazenado. 
-// Se o número atual for maior, atualize a variável.
-// *   Lembre-se de lidar com o caso de um array vazio.
-
-// ---
-
-const numeros = [55, 1, 10, 452, 421, 116];
-let numeroMaior = 0;
-
-function encontrarMaiorNumero(lista, numMaior) {
-    for (i of numeros) {
-    if (i > numeroMaior) {
-        numeroMaior = i
-        
-    }
+function encontrarMaiorNumero(lista) {
+    console.log(`Descubra o maior numero de uma lista: `)
+    let numeroMaior = 0;
+    for (i of lista) {
+        if (i > numeroMaior) {
+            numeroMaior = i
+        }
     }
     console.log(`O maior numero é ${numeroMaior}`)
 }
-encontrarMaiorNumero(numeros, numeroMaior)
+encontrarMaiorNumero(numeros)
---
+++ exercicio2.js
@@ -0,0 +1,14 @@
+// ## Exercício 2: Array e Loop - Soma de Elementos Pares
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1000]
+function somarPares(lista) {
+    let soma = 0
+    for (i of lista) {
+        if (i%2 == 0) {
+            soma += i
+        }
+    }
+    console.log(`A soma de todos os numeros pares da lista [${lista}] é: ${soma}`)
+}
+
+somarPares(numeros)
