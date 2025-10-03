2d173948a2a82f364c35ab9995d2b239665d38c6
Walmir1974 <walmir.vmartins@gmail.com>
2025-10-03 20:55:44 -0300

    Resolução do Exercício 02

---
+++ exercicio2.js
@@ -0,0 +1,15 @@
+function somarPares(numeros) {
+  let soma = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+      soma += numeros[i];
+    }
+  }
+
+  return soma;
+}
+
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(somarPares(numeros))
