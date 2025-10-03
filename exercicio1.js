35e3a0b52ad20b8d6749cc723c4b641860d9d461
deBokorni <afonssoleandro@gmail.com>
2025-10-02 21:47:11 -0300

    Quest 1

---
+++ exercicio1.js
@@ -0,0 +1,16 @@
+// Exercício 1: Array e Loop - Encontrar o Maior Número
+// Descrição
+// Crie uma função chamada `encontrarMaiorNumero` que recebe um array de números como argumento. A função deve retornar o maior número presente no array.
+
+const numeros = [12, 14, 16, 18, 999]
+let maiorNumero = 0
+
+function encontrarMaiorNumero(){
+    for (let i = 0; i <numeros.length; i++)
+        if (numeros[i] > maiorNumero){
+            maiorNumero = numeros[i]
+        }
+    console.log(maiorNumero)
+}
+
+encontrarMaiorNumero(numeros)
