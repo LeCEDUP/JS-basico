18d7115ee0c7a0ed5b033f9cbc49fcd80148419f
Idenilson27 <voigtidenilson@gmail.com>
2025-10-03 21:16:39 -0300

    altera.exer05

--- exercicio5.js
+++ exercicio5.js
@@ -1,25 +1,20 @@
-function encontrarProdutoMaisCaro(produtos) {
-  if (produtos.length === 0) {
-    return null; 
-  }
-
-  let produtoMaisCaro = produtos[0];
-
-  for (let i = 1; i < produtos.length; i++) {
-    if (produtos[i].preco > produtoMaisCaro.preco) {
-      produtoMaisCaro = produtos[i];
-    }
-  }
-
-  return produtoMaisCaro;
-}
-
-
 const produtos = [
   { nome: "Teclado Mecânico", preco: 350 },
   { nome: "Monitor Ultrawide", preco: 1500 },
   { nome: "Mouse Gamer", preco: 120 },
   { nome: "Webcam Full HD", preco: 200 }
 ];
+
+function encontrarProdutoMaisCaro(produtos) {
+  let produtoMaisCaro = produtos[0];
+
+  for (let i of produtos) {
+    if (i.preco > produtoMaisCaro.preco) {
+      produtoMaisCaro = i;
+    }
+  }
+
+  return produtoMaisCaro;
+}
 
 console.log(encontrarProdutoMaisCaro(produtos));
