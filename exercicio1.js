7cb5c49b15768802bc51d11d025fc5ad4baaa74b
miguelkik <miguelaparecidoribeiro06@gmail.com>
2025-10-06 16:06:22 +0000

    .

--- exercicio1.js
+++ exercicio1.js
@@ -1,7 +1,3 @@
 const numeros = [15, 8, 23, 4, 42, 16];
 
-const numerosPares = arrayParaProcessar.filter(function(numero) {
-    return numero % 2 === 0;
-  });
-  console.log("Números pares:", numerosPares);
-}
+console.log(numeros)
